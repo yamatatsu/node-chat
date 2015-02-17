7e5984ab3107ef94e9a863fd2c30993584cc6d43
山本達也 <yamamototatsuya@MBA-ikumigou.local>
2015-02-18 07:02:13 +0900

    チャット機能実装

--- index.js
+++ index.js
@@ -6,18 +6,15 @@ var port = process.env.PORT || 5000;
 
 //////////////////////
 // setting http
-app.set('port', port);
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-//  res.send('Hello World! welcome!');
 	res.render('index.jade');
 });
 
 var server = http.createServer(app);
 server.listen(port);
-console.log("Node app is running at localhost:" + app.get('port'));
-
+console.log("Node app is running at localhost:" + port);
 
 ///////////////////
 // setting ws
@@ -25,15 +22,34 @@ console.log("Node app is running at localhost:" + app.get('port'));
 var wss = new WebSocketServer({server:server});
 console.log("websocket server created");
 
-wss.on('connection', function(ws) {
-	var sendTimeTask = setInterval(function() {
-		ws.send(JSON.stringify(new Date()), function() {});
-	}, 1000);
+var userId = 1;
 
+// 接続開始
+wss.on('connection', function(ws) {
 	console.log('websocket connection open');
 
+	// clientに対してuserIdを振る
+	ws.send(JSON.stringify({
+		type: 'userId',
+		userId: userId
+	}));
+	userId++;
+
+	/**
+	 * ws受信時
+	 * メッセージを受けたら、時間を付与して、接続している全員にsend
+	 */
+	ws.on('message', function(json) {
+		console.log('message is ' + json);
+		var data = JSON.parse(json);
+		data.time = new Date();
+		wss.clients.forEach(function(client) {
+			client.send(JSON.stringify(data));
+		});
+	});
+
+	// クローズ
 	ws.on('close', function() {
 		console.log('websocket connection close');
-		clearInterval(sendTimeTask);
 	});
 });
